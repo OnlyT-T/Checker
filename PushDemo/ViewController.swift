3aef0cfecfd85a37c6a2bea17afc6bb8022f43a0
OnlyT-T <trungonlyt145@gmail.com>
2023-11-08 20:36:18 +0700

    commit PushDemo

---
+++ PushDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PushDemo
+//
+//  Created by Tran Thanh Trung on 08/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func pushTest() {
+        print("pushTest")
+    }
+
+
+}
+
